36b0bd9c59cd376217b9a4204e02c9d70955bc74
pomegranata <imeldamaretta.putri346@gmail.com>
2024-06-07 11:24:55 +0700

    Deploy

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-14.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"b38d630e","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"611202200053@mhs.dinus.ac.id","afterSubmitCcEmail":"611202200053@mhs.dinus.ac.id","afterSubmitBccEmail":"611202200053@mhs.dinus.ac.id","afterSubmitEmailSubject":"Masukan","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Silakan isi captcha di atas.","confirmationUrl":""}, '.uagb-block-b38d630e', 14 );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-65832876' );
+});
+ });
